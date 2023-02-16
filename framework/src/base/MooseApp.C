f57d0548920963f0fbd032792c6b6b80088344e4
Logan Harbour <loganhharbour@gmail.com>
2023-02-16 13:47:22 -0700

    Improve mesh property interface

--- framework/src/base/MooseApp.C
+++ framework/src/base/MooseApp.C
@@ -386,7 +386,6 @@ MooseApp::MooseApp(InputParameters parameters)
                                : nullptr),
     _execute_flags(moose::internal::getExecFlagRegistry().getFlags()),
     _automatic_automatic_scaling(getParam<bool>("automatic_automatic_scaling")),
-    _executing_mesh_generators(false),
     _popped_final_mesh_generator(false)
 {
 #ifdef HAVE_GPERFTOOLS
@@ -2159,8 +2158,6 @@ MooseApp::executeMeshGenerators()
   if (_mesh_generators.empty())
     return;
 
-  _executing_mesh_generators = true;
-
   createMeshGeneratorOrder();
 
   // set the final generator name
@@ -2205,14 +2202,9 @@ MooseApp::executeMeshGenerators()
       // Once we hit the generator we want, we'll terminate the loops (this might be the last
       // iteration anyway)
       if (_final_generator_name == name)
-      {
-        _executing_mesh_generators = false;
         return;
-      }
     }
   }
-
-  _executing_mesh_generators = false;
 }
 
 void
@@ -2822,4 +2814,25 @@ MooseApp::createRecoverablePerfGraph()
   return dynamic_cast<RestartableData<PerfGraph> &>(
              registerRestartableData("perf_graph", std::move(perf_graph), 0, false))
       .set();
+}
+
+bool
+MooseApp::constructedMeshGenerators() const
+{
+  return _action_warehouse.isTaskComplete("add_mesh_generator") &&
+         (_action_warehouse.hasTask("append_mesh_generator") &&
+          _action_warehouse.isTaskComplete("append_mesh_generator"));
+}
+
+bool
+MooseApp::constructingMeshGenerators() const
+{
+  return _action_warehouse.getCurrentTaskName() == "add_mesh_generator" ||
+         _action_warehouse.getCurrentTaskName() == "append_mesh_generator";
+}
+
+bool
+MooseApp::executingMeshGenerators() const
+{
+  return _action_warehouse.getCurrentTaskName() == "execute_mesh_generators";
 }
